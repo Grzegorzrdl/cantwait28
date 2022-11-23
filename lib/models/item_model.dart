580d04f1bacc31639c0e48de23db1b4955fd61ad
Grzegorz Radlinski <grzegorz1410@gmail.com>
2022-11-23 19:08:24 +0100

    Add Details Page

--- lib/models/item_model.dart
+++ lib/models/item_model.dart
@@ -4,9 +4,13 @@ class ItemModel {
     required this.title,
     required this.imageURL,
     required this.releaseDate,
-  });
+    });
   final String id;
   final String title;
   final String imageURL;
   final DateTime releaseDate;
+
+  String daysLeft() {
+    return releaseDate.difference(DateTime.now()).inDays.toString();
+  }
 }
